78c69d80e311b4636d5a2202d22bc39819950e71
Kellidan <kellidanfernandes57@gmail.com>
2026-09-02 12:46:47 +0530

    add change for config.py

--- config/database.py
+++ config/database.py
@@ -4,12 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-        # 'DATABASE_URL'
-        # , 
-        # 'mysql+pymysql://root:root@localhost/lms_db'
-    # )
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'uploads')
-    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB Limit
+    if not app.config.get('SQLALCHEMY_DATABASE_URI'):
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+
+    app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+    app.config.setdefault('UPLOAD_FOLDER', os.path.join(app.root_path, 'uploads'))
+    app.config.setdefault('MAX_CONTENT_LENGTH', 16 * 1024 * 1024)  # 16 MB Limit
+    
     db.init_app(app)
